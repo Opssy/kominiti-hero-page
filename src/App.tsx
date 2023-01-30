d6bc88892ee1c8335b3beb3626704a2cc1b444f8
Opssy <opeyemib39@gmail.com>
2023-01-30 12:16:44 +0100

    setup for kominiti hero section converted to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import Home from './pages/Home';
 import './App.css';
-import Home from "./pages/Home"
 
 function App() {
   return (
     <div className="App">
-        <Home/>
+      <Home/>
     </div>
   );
 }
